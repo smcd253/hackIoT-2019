68770bf142d098bfdf85b2528919c5814684106a
Spencer McDonough <shmcdono@usc.edu>
2019-03-23 23:24:35 -0700

    keypress

---
+++ keypress.py
@@ -0,0 +1,11 @@
+import readchar  # using module keyboard
+
+abc = 1
+
+c = readchar.readchar()
+print("acd" + c + "asd")
+
+while abc:  # making a loop
+    if str(c)==' ' :  # if key 'q' is pressed 
+    	print('You Pressed A Key!')
+        abc = 0  # finishing the loop
